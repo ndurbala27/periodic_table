4ca5e50c5775bbd5bff44e7afd0d9e5b205cadee
Nate Durbala <ndurbala27@gmail.com>
2024-03-15 19:05:38 +0000

    refactor:refactor variables

--- element.sh
+++ element.sh
@@ -2,7 +2,7 @@
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
-if [[ -z $1 ]] 
+if [[ -z $ELEM ]] 
   then
     echo "Please provide an element as an argument."
   else
